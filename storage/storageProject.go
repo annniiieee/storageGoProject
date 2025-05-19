9b5528b095a3d3da16b252037cdcca9b2357575a
annniiieee <annie.nguyenly@mail.concordia.ca>
2025-05-19 17:31:13 -0400

    tps and latency calculations added as new threads
    added sleeping time when simulating transactions for TPS calculations, needs buffer changes and output cleaning?

--- storage/storageProject.go
+++ storage/storageProject.go
@@ -10,7 +10,7 @@ import (
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func randomString() string {
-	b := make([]byte, rand.Intn(100))
+	b := make([]byte, rand.Intn(100)) // different legnths from 0 - 99
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
@@ -20,8 +20,6 @@ func randomString() string {
 type transaction struct {
 	id        string // generate a random string
 	startTime time.Time
-	endTime   time.Time
-	latency   time.Duration // (endTime.Sub(startTime))
 }
 
 func newTransaction() transaction {
@@ -31,40 +29,82 @@ func newTransaction() transaction {
 	}
 }
 
+type systemState struct {
+	totalTransactions int
+	//latencies          []time.Duration
+	transactionStorage map[string]time.Duration // [transaction.id, transaction.latency]
+}
+
 func main() {
 	transactionChannel := make(chan transaction, 100) // buffer amount should depend on TPS
-	transactionStorage := make(map[string]transaction)
+	systemState := systemState{
+		transactionStorage: make(map[string]time.Duration),
+	}
 	var mutex sync.Mutex
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	go func() { //anonymous function using goroutine
+	// thread to calculate latency
+	go func() {
+		//call .Done no matter what happens to goroutine
 		defer wg.Done() // placed at the beginning to keep it safe from early return & panic
 		for t := range transactionChannel {
+			latency := time.Since(t.startTime)
+			// add to storage
 			mutex.Lock()
-			transactionStorage[t.id] = t
-			t.endTime = time.Now()
-			t.latency = t.endTime.Sub(t.startTime)
+			systemState.transactionStorage[t.id] = latency
+			systemState.totalTransactions++
 			mutex.Unlock()
 
-			fmt.Printf("Stored transaction with ID %s and latency of %s\n", t.id, t.latency)
-			time.Sleep(500 * time.Millisecond)
+			fmt.Printf("Stored transaction with ID %s and latency of %s\n", t.id, latency)
+		}
+		fmt.Printf("Total transactions in system: %d\n", systemState.totalTransactions)
+
+	}()
+
+	wg.Add(1)
+	// TPS calculation thread
+	go func() {
+		defer wg.Done()
+		// timers = countdown, so use tickers
+		// tickers for repeatedly events at reg intervals in future
+		ticker := time.NewTicker(1 * time.Second) // bc tpS
+		defer ticker.Stop()                       // calls .Stop no matter what
+		var lock sync.Mutex
+		var lastTotal int
+
+		// tps = current - last
+		for range ticker.C {
+			lock.Lock()
+			tps := (systemState.totalTransactions - lastTotal)
+			lastTotal = systemState.totalTransactions
+			lock.Unlock()
+
+			fmt.Printf("TPS %d\n", tps)
+			if tps == 0 {
+				break
+			}
 		}
 	}()
 
-	//simulating transaction enterring the channel
-	for i := 0; i < 10; i++ {
+	//simulating transaction enterring the channel using threads
+	var senderWg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		senderWg.Add(1)
 		var t transaction
 		t = newTransaction()
 
-		transactionChannel <- t
-		fmt.Printf("Transaction in channel with ID: %s\n", t.id)
-
+		go func() {
+			defer senderWg.Done()
+			transactionChannel <- t
+			fmt.Printf("Transaction in channel with ID: %s\n", t.id)
+		}()
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond) // random simulating "wait time" bc transactions were saved within milliseconds making TPS = max range
 	}
 
+	senderWg.Wait()
 	// After sending all transactions
 	close(transactionChannel) // let goroutine know there's no more input
-	// time.Sleep(time.Second) ///// blindly delaying the goroutines is so bad
+	// time.Sleep(time.Second) ///// blindly delaying the goroutines is so bad use waitgroup instead
 	wg.Wait()
-
 }
